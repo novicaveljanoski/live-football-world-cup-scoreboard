63e3dcda634e349af72b710bea64ed08f90454e4
novicaveljanoski <novicaveljanoski12@gmail.com>
2026-03-17 22:52:40 +0100

    feat: implement update score with input validation

--- src/main/java/com/worldcup/Scoreboard.java
+++ src/main/java/com/worldcup/Scoreboard.java
@@ -18,6 +18,23 @@ public class Scoreboard {
         matches.add(new Match(homeTeam, awayTeam));
     }
 
+    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        if (homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException("Scores cannot be negative");
+        }
+
+        Match match = null;
+        for (Match m : matches) {
+            if (m.getHomeTeam().equals(homeTeam) && m.getAwayTeam().equals(awayTeam)) {
+                match = m;
+                break;
+            }
+        }
+        if (match == null) throw new IllegalArgumentException("Match not found");
+
+        match.updateScore(homeScore, awayScore);
+    }
+
     public List<Match> getSummary() {
         return matches;
     }
--- src/test/java/com.worldcup/ScoreboardTest.java
+++ src/test/java/com.worldcup/ScoreboardTest.java
@@ -46,4 +46,32 @@ public class ScoreboardTest {
     void shouldThrowExceptionWhenAwayTeamIsEmpty() {
         assertThrows(IllegalArgumentException.class, () -> scoreboard.startMatch("Mexico", ""));
     }
+
+    @Test
+    void shouldUpdateScore() {
+        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.updateScore("Mexico", "Canada", 1, 0);
+
+        Match match = scoreboard.getSummary().getFirst();
+
+        assertEquals(1, match.getHomeScore());
+        assertEquals(0, match.getAwayScore());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingNonExistentMatch() {
+        assertThrows(IllegalArgumentException.class, () -> scoreboard.updateScore("Mexico", "Canada", 1, 0));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenHomeScoreIsNegative() {
+        scoreboard.startMatch("Mexico", "Canada");
+        assertThrows(IllegalArgumentException.class, () -> scoreboard.updateScore("Mexico", "Canada", -1, 0));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAwayScoreIsNegative() {
+        scoreboard.startMatch("Mexico", "Canada");
+        assertThrows(IllegalArgumentException.class, () -> scoreboard.updateScore("Mexico", "Canada", 0, -1));
+    }
 }
